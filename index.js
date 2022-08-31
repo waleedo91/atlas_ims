ab4c8dcac8f5ab73093166a83362c228f7cdedac
Waleed <wsaleh1991@gmail.com>
2022-08-30 20:30:54 -0500

    function for form once submitted

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+let button = document.getElementById("form-button");
+let form = document.getElementById("contact-us");
+let thanks = document.getElementById("change-after");
+
+button.onclick = function (e) {
+  e.preventDefault();
+  form.style.visibility = "hidden";
+  thanks.innerHTML = "Thank you for contacting us";
+};
