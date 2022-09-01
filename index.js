d63ec9aac93465bca95cc937b3cbf8049425fb5d
Sami Samara <samisamara99@yahoo.com>
2022-09-01 00:51:07 -0500

    updated whyUs desktop ui

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let button = document.getElementById("form-button");
 let form = document.getElementById("contact-us");
 let thanks = document.getElementById("change-after");
+let temp = document.querySelector(".form-ui")
 
 button.onclick = function (e) {
   e.preventDefault();
